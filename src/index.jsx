6e61295edfa5c87c4e6ae688b8f876fa80fa9f3a
tangzixiang <tomson_tang@yeah.net>
2017-08-10 19:29:00 +0800

    更新 react-router-redux-saga-model API

--- src/index.jsx
+++ src/index.jsx
@@ -2,19 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Route} from 'react-router';
 import {Link} from 'react-router-dom';
-import {BrowserRouterControllerProvider} from 'react-router-redux-saga-model'
+import {BrowserRouterProvider} from 'react-router-redux-saga-model'
 import About from './view/about/index.jsx';
 import Index from './view/index/index.jsx';
 
 
 ReactDOM.render(
-  <BrowserRouterControllerProvider>
+  <BrowserRouterProvider>
     <div>
         <Link to="/about">关于</Link>
         <Link to="/">主页</Link>
         <Route exact path="/" component={Index}/>
         <Route path="/about" component={About}/>
     </div>
-  </BrowserRouterControllerProvider>,
+  </BrowserRouterProvider>,
   document.querySelector('#root')
-)
+);
